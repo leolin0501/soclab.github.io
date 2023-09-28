8b7efd04358a7740aad6ae04a9c518b147cd4587
leolin0501 <145218745+leolin0501@users.noreply.github.com>
2023-09-28 19:22:46 +0800

    Add files via upload

---
+++ Lab1/MultipTester.cpp
@@ -0,0 +1,49 @@
+
+
+//用於測試 Multiplication.h 中的 multip_2num 函式的測試程式（testbench）。該測試程式通過將不同的整數對傳遞給 multip_2num 函式，並檢查其返回值是否等於相應的整數乘積，來測試 multip_2num 函式的正確性。
+#include <iostream>
+#include "Multiplication.h"//以便在程式中使用標頭檔"Multiplication.h"相關的定義和宣告。
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int pass = 1; //用於記錄測試是否通過，初始值為 1，表示通過。
+	int n32Multip; //用於存儲 multip_2num 函式的返回值，即整數乘積。
+
+	cout << ">> Start test!" << endl;//使用 cout 將開始測試的訊息輸出到終端。
+
+	for(i = 1; i < 10; i++) {//持續繼續下一輪的迴圈，直到測試完所有整數對。
+		cout << "------------------------" << endl;
+		for(j = 1; j < 10; j++) {
+			multip_2num(i, j, &n32Multip);//在迴圈內部，使用 multip_2num 函式計算 i 和 j 的乘積，並將結果存儲在 n32Multip 中。
+
+			cout << i << " * " << j << " = " << n32Multip << endl;//使用 cout 將計算結果輸出到終端，包括 i、j 和 n32Multip 的值。
+			if(n32Multip != (i * j)) {//檢查計算結果是否等於 i * j，如果不等於，則將 pass 設置為 0，表示測試未通過。
+				pass = 0;
+			}
+		}
+	}
+	cout << "------------------------" << endl;//測試完成後，輸出分隔線和測試結果訊息。
+
+	if(!pass) {
+		cout << ">> Test failed!" << endl;//最終檢查 pass 的值，如果為 0，表示測試未通過，輸出測試失敗的訊息，並返回錯誤碼 1。如果 pass 仍然為 1，表示測試通過，輸出測試通過的訊息，並返回正確碼 0。
+		return 1;
+	}
+
+	cout << ">> Test passed!" << endl;
+	cout << "------------------------" << endl;
+	return 0;
+}
+
+
+/*cout << ">> Start test!" << endl;
+
+cout：cout 是 C++ 標準程式庫中的標準輸出流，用於向終端窗口輸出資訊。
+
+<<：這是輸出流運算子，用於將右側的內容輸出到左側的流中。在這個情況下，它將字串 ">> Start test!" 輸出到 cout 流中。
+
+">> Start test!"：這是要輸出的字串，它包含了訊息內容。
+
+endl：endl 是 C++ 標準程式庫中的標準輸出結束符號，它表示換行並刷新輸出緩衝區。當 endl 被使用時，它會將之前的內容輸出到終端，然後換行。這有助於確保訊息被立即顯示在終端窗口中。*/
---
+++ Lab1/Multiplication.cpp
@@ -0,0 +1,32 @@
+
+#include "Multiplication.h" //以便在程式中使用標頭檔"Multiplication.h"相關的定義和宣告
+
+void multip_2num(int32_t n32In1, int32_t n32In2, int32_t* pn32ResOut) //它接受兩個32位整數 (n32In1 和 n32In2) 作為輸入，並接受一個指向32位整數的指針 (pn32ResOut) 作為輸出。
+{
+
+//#pragma HLS INTERFACE 指令，它們用於定義接口屬性。
+//#pragma HLS：這是 Vivado HLS 工具支援的指示性指令的開始。
+//INTERFACE：這部分指定了這個指令是用來設置函式的介面屬性。
+#pragma HLS INTERFACE s_axilite port=pn32ResOut //這個指令指定 pn32ResOut 參數用來傳遞一個32位整數的結果，作為 AXI Lite (一種硬體協定)接口的一部分，這是一種用於訪問外部硬體的標準接口。
+#pragma HLS INTERFACE s_axilite port=n32In2 //這兩個指令指定 n32In2 和 n32In1 參數用來接收輸入的整數值，也作為 AXI Lite 接口的一部分。
+#pragma HLS INTERFACE s_axilite port=n32In1//這行程式碼執行兩個輸入整數 n32In1 和 n32In2 的乘法運算，然後將結果存儲在指針 pn32ResOut 指向的位置。
+
+
+//下面這行是用於指定函式的控制信號介面的設定，特別是用於控制硬體加速器的開始和停止。
+//"ap_ctrl_none"是xilinx的保留字元，指令告訴Vivado HLS不生成開始和停止硬體加速器的控制邏輯。如果在synthesis時需要硬體加速器，那麼需要這些控制信號來適當地開始和停止硬體運行。如果沒有這些控制信號，可能會導致硬體加速器無法正確啟動或停止，從而導致錯誤。
+//Vivado HLS可能會根據指令的依賴關係生成硬體邏輯。如果移除 ap_ctrl_none，可能會影響到指令的依賴關係，導致錯誤。
+//跑C simulation、Csynthesis時要註解，cosimulation再解註解。
+//port=return 中的 port 是一個合法的識別符號，用於指定你想要針對的函式參數，而 return 則是函式的返回值。這是 Vivado HLS 中的一種常見用法，用於指示你想要配置的介面屬性是與函式的返回值相關的。
+#pragma HLS INTERFACE ap_ctrl_none port=return 
+
+
+
+	*pn32ResOut = n32In1 * n32In2;
+
+	return;
+}
+
+
+//依賴關係：硬體元件或指令之間的相互關係
+//1.數據依賴關係：這種依賴關係發生在當一個元件需要使用另一個元件的輸出數據時。
+//2.控制依賴關係：這種依賴關係與硬體的控制流程有關。當一個元件的操作受到另一個元件的控制信號或控制命令的影響時
